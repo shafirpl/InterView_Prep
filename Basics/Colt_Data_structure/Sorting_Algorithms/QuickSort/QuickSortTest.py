99d4376b0d06c0916c5a846bb12e449a35d8e8c1
shafirpl <shafi.rpl@gmail.com>
2020-02-04 00:55:12 -0800

    Changed tests

--- Basics/Colt_Data_structure/Sorting_Algorithms/QuickSort/QuickSortTest.py
+++ Basics/Colt_Data_structure/Sorting_Algorithms/QuickSort/QuickSortTest.py
@@ -8,6 +8,7 @@ class BubbleSortTest(unittest.TestCase):
         self.assertEqual(quickSort([10,5,90,0,10]), [0,5,10,10,90])
         self.assertEqual(quickSort([64, 34, 25, 22, 11, 90,12]), [11,12,22,25,34,64,90])
         self.assertEqual(quickSort([8,90,4,1,5,5]), [1,4,5,5,8,90])
+        self.assertEqual(quickSort([1, 2, 3, 4, 5, 5]), [1, 2, 3, 4, 5, 5])
 
 
 if __name__ == "__main__":
