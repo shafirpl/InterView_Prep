69e6348998b63a072032eed32a5d1720870d17da
shafirpl <shafi.rpl@gmail.com>
2020-05-17 05:54:39 -0700

    Colt dynamic done

--- Basics/Colt_Data_structure/DynamicPrograming/fibonaci.py
+++ Basics/Colt_Data_structure/DynamicPrograming/fibonaci.py
@@ -18,10 +18,25 @@ def fibonaci_memo(n, memo = {}):
 
     return (fibonaci_memo(n-1, memo) + fibonaci_memo(n-2, memo))
 
-def fibonaci_tabulatiob
+def fibonaci_tabulation(n):
+    if( n<= 2 ):
+        return 1
+    memo = {}
+    memo[0] = 0
+    memo[1] = 1
+    memo [2] = 1
+    i = 3
+
+    while i <= n:
+        memo[i] = memo[i-1] + memo[i-2]
+        i += 1
+
+    return memo[n]
 
 
 
-print(fibonaci_memo(100))
+# print(fibonaci_memo(100))
+# print(fibonaci_memo(100))
+print(fibonaci_tabulation(100)- fibonaci_memo(100))
 
 # print(fibonaci_recursive(100))
